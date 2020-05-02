87c777e87cffba1141c49ce588d2e7e6d1fe54b7
Siqiao Li <11291141bjtu@gmail.com>
2020-05-02 12:21:06 +1000

    welcome

---
+++ src/main/java/com/videoweb/ccassign2/Ccassign2Application.java
@@ -0,0 +1,14 @@
+package com.videoweb.ccassign2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Ccassign2Application {
+
+	public static void main(String[] args) {
+
+		SpringApplication.run(Ccassign2Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/videoweb/ccassign2/Ccassign2ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.videoweb.ccassign2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Ccassign2ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
